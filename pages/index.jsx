1757d9d31f707b69677dee0a226942af0c833e1e
Felipe Gabriel <85371036+gabriewf@users.noreply.github.com>
2022-03-09 18:42:52 -0300

    Commit 17

--- pages/index.jsx
+++ pages/index.jsx
@@ -96,7 +96,7 @@ export default function Home() {
               </a>
               {/* Youtube */}
               <a
-                href="https://www.youtube.com"
+                href="https://www.youtube.com/channel/UChVBw8q75tPAdRkfWWgNm0w"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="ml-3 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
