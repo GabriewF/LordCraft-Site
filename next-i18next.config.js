fd713130e72c502684df888bd5665550719e93b8
gabriewf <feegasaa@gmail.com>
2022-03-10 16:37:40 -0300

    Added Japanese locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const { resolve } = require("node:path")
 module.exports = {
   i18n: {
     defaultLocale: "pt",
-    locales: ["pt", "en", "es", "de", "fr"],
+    locales: ["pt", "en", "es", "de", "fr", "ja"],
     localePath: resolve("./locales"),
   },
 };
