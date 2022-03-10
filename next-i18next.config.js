27a7fc7b3f6101027ba4a07211370e7a53e6d516
gabriewf <feegasaa@gmail.com>
2022-03-10 12:10:04 -0300

    Added serverSideError and French translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const { resolve } = require("node:path")
 module.exports = {
   i18n: {
     defaultLocale: "pt",
-    locales: ["pt", "en", "es", "de"],
+    locales: ["pt", "en", "es", "de", "fr"],
     localePath: resolve("./locales"),
   },
 };
---
+++ pages/500.jsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export default function Custom500() {
+  const { t } = useTranslation("serverSideError");
+
+  return (
+    <container>
+      <Head>
+        {/* Metadata */}
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta property="og:title" content="500" />
+        <meta
+          property="og:description"
+          content="O 'LordCraft' é um servidor survival na ultima versão do Minecraft, com propiedade do streamer LordLIVE_ e é hospedado pela PurpleHost"
+        />
+        <meta property="og:site_name" content="LordCraft" />
+        <meta property="og:url" content="https://www.lord-craft.tk/404"></meta>
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:title" content="500" />
+        <meta
+          name="twitter:description"
+          content="O 'LordCraft' é um servidor survival na ultima versão do Minecraft, com propiedade do streamer LordLIVE_ e é hospedado pela PurpleHost"
+        />
+        <meta name="twitter:site" content="@LordCraft" />
+        <meta name="twitter:url" content="https://www.lord-craft.tk/404" />
+        <meta name="twitter:image" content="../public/creeper.webp" />
+
+        {/* Title */}
+        <title>{t("title")}</title>
+      </Head>
+      <div id="Container">
+        <section>
+          <div
+            id="bg"
+            className="w-screen h-screen animation-pulse bg-gradient-to-r from-sky-500 to-indigo-500 dark:bg-gradient-to-r dark:from-sky-700 dark:to-indigo-700 grid grid-cols-1 gap-2 place-content-center"
+          >
+            <div id="title" className="text-5vw">
+              <h1 className="font-MinecraftFiveBold flex justify-center items-center text-justify text-slate-200">
+                {t("titleText")}
+              </h1>
+              <p className="font-MinecraftTen flex justify-center items-center text-justify text-slate-200">
+                {t("titleDesc")}
+              </p>
+
+              <div id="linkButtons">
+                <p className="font-MinecraftFiveBold text-3vw flex justify-center content-center text-slate-200">
+                  {/* Go back */}
+                  <Link
+                    className="flex justify-center content-center text-slate-200"
+                    href="/"
+                  >
+                    <a
+                      className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      aria-current="page"
+                    >
+                      <button className="bg-gray-50 hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                        <Image
+                          className="fill-current w-4 h-4 mr-2"
+                          width="16"
+                          height="16"
+                          alt="Back"
+                          src="/images/back.svg"
+                        />
+                        <span className="ml-2 text-1.5vw font-MinecraftFiveBold">
+                          {t("goBack")}
+                        </span>
+                      </button>
+                    </a>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </container>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["serverSideError"])),
+    },
+  };
+}
