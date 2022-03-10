8c78e32bfef59d6e13c5440bea950386b0eb5314
gabriewf <feegasaa@gmail.com>
2022-03-10 11:49:05 -0300

    Added Deutsch and Spanish locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const { resolve } = require("node:path")
 module.exports = {
   i18n: {
     defaultLocale: "pt",
-    locales: ["pt", "en"],
+    locales: ["pt", "en", "es", "de"],
     localePath: resolve("./locales"),
   },
 };
